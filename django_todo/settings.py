287d61f2fc218808f1a74fcd9e40ee9cfddfb83e
elliotjonesdev <elliotjonesdev@gmail.com>
2023-09-26 20:47:24 +0000

    add form to create an item

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.exists("env.py"):
+  import env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-40s8sotyy=fey6yfq&-0$zs28=3rly@kq!k+$ox%vh4z%ec+sb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-elliotjones-cifsfhellod-dzggvnd3ms6.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-elliotjones-cifsfhellod-dzggvnd3ms6.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
