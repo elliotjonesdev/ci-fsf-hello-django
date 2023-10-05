8f9565385648a0655189b9425315440856292426
elliotjonesdev <elliotjonesdev@gmail.com>
2023-10-05 20:09:27 +0000

    Test

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-elliotjones-cifsfhellod-dzggvnd3ms6.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['ej1976-django-todo-app-80a6b4914f6a.herokuapp.com']
 
 
 # Application definition
@@ -88,6 +88,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 DATABASES = {
    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+   print("Connected")
 }
 
 
